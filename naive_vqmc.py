65d01020d23ef0d10d17022c1410567090fdbff1
Ricky <patricknave95@gmail.com>
2020-04-29 16:27:18 -0400

    Moved images to subdirectory.  Added MCMC diagnostic function.

--- naive_vqmc.py
+++ naive_vqmc.py
@@ -77,11 +77,159 @@ def UniformMetropolisSampler(a, b, M, delta=0.2):
 		approx += Ecurrent  # This must come before the variance update b/c of the convention adopted in the 'SampleVarianceLocalEnergy' function
 		variance = SampleVarianceLocalEnergy(Ecurrent, (approx / (i+1)), variance, i+1)
 
-		if(i%1000 == 0):
+		if(i%10000 == 0):
 			print("Variance at i=" + str(i) + ":  " + str(variance))
 
 	print("For a,b = " + str(a) + ", " + str(b) +": acceptance rate = " + str(numberOfAcceptedMoves / M))
 	return approx / M 
+
+
+
+
+'''
+This function approximates the integral <psi(a,b)|H|psi(a,b)> by using the Metropolis-Hastings algorithm where the proposal probability (for moves in 2-electron position space) is uniform over the hypercube delta * [-1,1]^6.  M samples are taken for this approximation.
+
+This function differs from 'UniformMetropolisSampler' in that this function is designed to help diagnose the mcmc convergence.  
+'''
+def DiagnosticMetropolisSampler(a, b, M, delta=0.2, measurementSpacing = 100, numChains=3):
+	
+	chainEstimateValues = []			# an array of arrays; each element array holds the estimated (total) energy after various numbers of monte carlo steps
+	
+	chainIntegrandValues = []			# an array of arrays; each element array holds the sampled values of integrand (local energy) after various numbers of monte carlo steps
+	
+	measurementIndices = []				# the steps at which measurements are taken (zero-indexed)
+	index = measurementSpacing
+	while(index <= M):
+		measurementIndices.append(index)
+		index += measurementSpacing
+	
+	for chain in range(numChains):
+
+		# initialize energy approximation 
+		#  (i.e. approximation of the integral <psi(a,b)|H|psi(a,b)> )
+		#energies
+		approx = 0
+	
+		measurementValues = []		# estimated energy
+		#measurementIndices = []
+		measurementLocal = []		# actual local energy measurements
+	
+		# initialize approximation of local energy variance
+		variance = 0
+	
+		# randomly initialize in [-1,1]^6 (2-electron position space)
+		v1 = RandomVector(1.0)
+		v2 = RandomVector(1.0)
+	
+		r1 = norm(v1)
+		r2 = norm(v2)
+		r12 = norm(v1 - v2)
+	
+		Ecurrent = LocalEnergy(a, b, v1, v2, r1, r2, r12)
+		PseudoDensityCurrent = ProportionalToPi(a, b, v1, v2)
+		
+		numberOfAcceptedMoves = 0
+	
+		for i in range(M):
+			v1proposed = v1 + RandomVector(delta)
+			v2proposed = v2 + RandomVector(delta)
+	
+			r1proposed = norm(v1proposed)
+			r2proposed = norm(v2proposed)
+			r12proposed = norm(v1proposed - v2proposed)
+	
+			Eproposed = LocalEnergy(a, b, v1proposed, v2proposed, r1proposed, r2proposed, r12proposed)
+			PseudoDensityProposed = ProportionalToPi(a, b, v1proposed, v2proposed)
+	
+			if MetropolisAcceptance(PseudoDensityCurrent, PseudoDensityProposed):
+				v1 = v1proposed
+				v2 = v2proposed
+				r1 = r1proposed
+				r2 = r2proposed
+				r12 = r12proposed
+				Ecurrent = Eproposed
+				PseudoDensityCurrent = PseudoDensityProposed
+	
+				numberOfAcceptedMoves += 1
+	
+			approx += Ecurrent  # This must come before the variance update b/c of the convention adopted in the 'SampleVarianceLocalEnergy' function
+			variance = SampleVarianceLocalEnergy(Ecurrent, (approx / (i+1)), variance, i+1)
+	
+			if((i+1)%measurementSpacing == 0):
+				measurementValues.append(approx / (i+1))
+				#measurementIndices.append(i)
+				measurementLocal.append(Ecurrent)
+	
+			if(i%10000 == 0):
+				print("Variance at i=" + str(i) + ":  " + str(variance))
+		
+		chainEstimateValues.append(measurementValues)
+		chainIntegrandValues.append(measurementLocal)
+		#chainIndices.append(measurementIndices)
+
+
+	# Plot estimated energy vs monte carlo steps
+
+	fig = plt.figure(1)
+	#ax = plt.axes(projection='3d')
+	ax = plt.axes()
+
+	for chain in range(numChains):
+		measurementValues = chainEstimateValues[chain]
+		#measurementIndices = chainIndices[chain]
+		ax.plot(measurementIndices, measurementValues)
+
+	title = r"Estimated Wavefunction Energy ($\alpha$=" + str(a) + r", $\beta$=" + str(b) + ")"
+	ax.set_title(title)
+	ax.set_xlabel("Monte Carlo Steps")
+	ax.set_ylabel("Energy (Hartrees)")
+	
+
+	plt.show()
+
+	now = datetime.now()
+	date_time = now.strftime("%Y-%m-%d-%H-%M-%S")
+
+	figname = "./images/MC_EstimatedEnergyPlot_a=" + str(a) + "_b=" + str(b) + "_M=" + str(M) + "_delta=" + str(delta) + "_time=" + date_time + ".png"
+	fig.savefig(figname, dpi=fig.dpi)
+	#fig.savefig(figname)
+	
+	
+	# Plot local energy (integrand) vs monte carlo steps
+	
+	fig2 = plt.figure(2)
+	#ax = plt.axes(projection='3d')
+	ax2 = plt.axes()
+	
+
+	for chain in range(numChains):
+		measurementValues = chainIntegrandValues[chain]
+		#measurementIndices = chainIndices[chain]
+		ax2.plot(measurementIndices, measurementValues)
+		#print("local energy measurements = " + str(len(measurementValues)))
+		#print("numIndices = " + str(len(measurementIndices)))
+
+	title = r"Local Energy ($\alpha$=" + str(a) + r", $\beta$=" + str(b) + ")"
+	ax2.set_title(title)
+	ax2.set_xlabel("Monte Carlo Steps")
+	ax2.set_ylabel("Local Energy")
+	
+
+	plt.show()
+
+	now = datetime.now()
+	date_time = now.strftime("%Y-%m-%d-%H-%M-%S")
+
+	figname = "./images/MC_LocalEnergyPlot_a=" + str(a) + "_b=" + str(b) + "M=" + str(M) + "_delta=" + str(delta) + "_time=" + date_time + ".png"
+	fig2.savefig(figname, dpi=fig.dpi)
+	#fig.savefig(figname)
+	
+
+	print("For a,b = " + str(a) + ", " + str(b) +": acceptance rate = " + str(numberOfAcceptedMoves / M))
+	
+	
+	return approx / M 
+
 
 
 '''
@@ -390,7 +538,8 @@ def AdaptiveGridSearch(amax=5, bmax=5, M=1000, initialDelta=0.2):
 
 	metastring = "Approx_Energy_M=" + str(M) + "_delta=Variable_a=0-" + str(amax) + "_b=0-" + str(bmax) + "_timestamp=" + date_time
 	figname = "Graph_3D_" + metastring + ".png"
-	fig.savefig(figname, dpi=fig.dpi)
+	#fig.savefig(figname, dpi=fig.dpi)
+	fig.savefig(figname)
 
 	# save data
 	datastring = "Grid_" + metastring + ".csv"
@@ -407,7 +556,7 @@ if __name__ == "__main__":
 	print("Setting up variational quantum monte carlo for Helium....")
 	
 
-	M = 50000
+	M = 250000
 	delta = 0.2
 	print("M = " + str(M) + ", delta = " + str(delta))
 
@@ -415,4 +564,7 @@ if __name__ == "__main__":
 	#results = AdaptiveGridSearch(5.0, 5.0, M, delta)
 
 	# do a local search to refine values of alpha, beta
-	results = FineGridSearch(1.6, 1.9, 20, 0.2, 0.5, 20, M, delta)
+	#results = FineGridSearch(1.6, 1.9, 20, 0.2, 0.5, 20, M, delta)
+
+	# test MC convergence
+	a = DiagnosticMetropolisSampler(0.1, 0.3, M, delta, 1000, 5)
